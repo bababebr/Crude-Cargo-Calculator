632b1d36b02abe63207eac556238b282edde6f18
barabebr <unior1234@yandex.ru>
2023-07-20 05:49:53 +0600

    refactor: Density

--- src/main/java/com/example/oct/units/api/Api.java
+++ src/main/java/com/example/oct/units/api/Api.java
@@ -1,10 +1,20 @@
 package com.example.oct.units.api;
 
+import com.sun.xml.bind.v2.TODO;
 import lombok.Data;
 
 @Data
 public class Api {
 
+    /**
+     * TODO
+     * Refactor Api/Dens
+     *  - Api
+     *  - Dens Vac
+     *  - Dens Air
+     *  - Dens Temp
+     *  - Specific Gravity
+     */
     private double api;
     private double specificGravity;
     private double densAir;
@@ -14,15 +24,16 @@ public class Api {
         Api a = new Api();
         a.setApi(api);
         a.setSpecificGravity(a.toSpecGravity(api));
-        a.setDensAir(a.getSpecificGravity() - 0.0011);
+        a.setDensAir(a.densVac - 0.0011);
         return a;
     }
 
     public static Api formDens(double densVac) {
         Api a = new Api();
-        a.setSpecificGravity(densVac);
+        a.setDensVac(densVac);
         a.setApi(a.toApi(densVac));
-        a.setDensAir(a.getSpecificGravity() - 0.0011);
+        a.setSpecificGravity(a.toSpecGravity(a.api));
+        a.setDensAir(a.densVac - 0.0011);
         return a;
     }
 
@@ -32,5 +43,9 @@ public class Api {
 
     private double toSpecGravity(double api) {
         return 141.5d / (131.5d + api);
+    }
+
+    public double fromApiToDensVac(double api) {
+
     }
 }
