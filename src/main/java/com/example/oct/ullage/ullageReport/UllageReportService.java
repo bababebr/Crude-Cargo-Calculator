19e7d1627c2735ddee873d52a693a3717f64e846
barabebr <unior1234@yandex.ru>
2023-07-20 07:14:14 +0600

    refactor: Api class

--- src/main/java/com/example/oct/ullage/ullageReport/UllageReportService.java
+++ src/main/java/com/example/oct/ullage/ullageReport/UllageReportService.java
@@ -34,6 +34,6 @@ public class UllageReportService {
         } else {
 
         }
-
+        return null;
     }
 }
--- src/main/java/com/example/oct/units/api/Api.java
+++ src/main/java/com/example/oct/units/api/Api.java
@@ -2,19 +2,12 @@ package com.example.oct.units.api;
 
 import com.sun.xml.bind.v2.TODO;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class Api {
 
-    /**
-     * TODO
-     * Refactor Api/Dens
-     *  - Api
-     *  - Dens Vac
-     *  - Dens Air
-     *  - Dens Temp
-     *  - Specific Gravity
-     */
     private double api;
     private double specificGravity;
     private double densAir;
@@ -24,6 +17,7 @@ public class Api {
         Api a = new Api();
         a.setApi(api);
         a.setSpecificGravity(a.toSpecGravity(api));
+        a.setDensVac(a.fromApiToDensVac(api));
         a.setDensAir(a.densVac - 0.0011);
         return a;
     }
@@ -31,8 +25,8 @@ public class Api {
     public static Api formDens(double densVac) {
         Api a = new Api();
         a.setDensVac(densVac);
-        a.setApi(a.toApi(densVac));
-        a.setSpecificGravity(a.toSpecGravity(a.api));
+        a.setSpecificGravity(a.fromDensVacToSpecificGravity(densVac));
+        a.setApi(a.toApi(a.specificGravity));
         a.setDensAir(a.densVac - 0.0011);
         return a;
     }
@@ -46,6 +40,31 @@ public class Api {
     }
 
     public double fromApiToDensVac(double api) {
+        double specGravity = toSpecGravity(api);
+        if (api > 94.88) return specGravity;
+        if (api > 76.54) return specGravity - 0.0001;
+        if (api > 59.65) return specGravity - 0.0002;
+        if (api > 46.86) return specGravity - 0.0003;
+        if (api > 32.95) return specGravity - 0.0004;
+        if (api > 16.74) return specGravity - 0.0005;
+        if (api > 3.31) return specGravity - 0.0006;
+        else return specGravity - 0.0007;
 
+    }
+
+    public double fromDensVacToSpecificGravity(double densVac) {
+        if (densVac > 0.74 && densVac < 0.793) return densVac + 0.0003;
+        if (densVac >= 0.793 && densVac < 0.86) return densVac + 0.0004;
+        if (densVac >= 0.86 && densVac < 0.954) return densVac + 0.0005;
+        if (densVac >= 0.954 && densVac < 1.049) return densVac + 0.0006;
+        else return densVac + 0.0007;
+
+    }
+
+    public static void main(String[] args) {
+        Api api1 = Api.fromApi(25);
+        System.out.println(api1);
+        Api api2 = Api.formDens(0.91);
+        System.out.println(api2);
     }
 }
